10eea2672efadac440549011d4a831423723baae
dibakar102 <dibakar102@yahoo.com>
2016-11-28 20:12:02 -0800

    Update FakeHello.java

--- src/FakeHello.java
+++ src/FakeHello.java
@@ -3,5 +3,6 @@ public class FakeHello {
 	public static void main(String[] args){
 		System.out.println("Hello fake world");
 		System.out.println("Sam loves men");
+		System.out.println("Chris a porn star");
 	}
 }
